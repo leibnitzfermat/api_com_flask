c0885d82ff2ee3582162e2f01d8168678374846c
Leibnitz Fermat <leibnitzfsa@gmail.com>
2022-03-31 22:44:09 -0300

    add consulta dos nomes dos hoteis

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
-from resources.hotel import Hoteis
+from resources.hotel import Hoteis, Hotel, Lista_Hoteis
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Lista_Hoteis, '/lista_hoteis')
+api.add_resource(Hotel, '/hoteis/<string:id_hotel>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,23 +1,24 @@
+from os import sep
 from flask_restful import Resource
 
 
 hoteis = [
     {
-        'hotel_id': 'alpha',
+        'id_hotel': 'alpha',
         'nome': 'Alpha Hotel',
         'estrelas': 4.3,
         'diaria': 420.34,
         'cidade': 'Belo Horizonte'
     },
     {
-        'hotel_id': 'bravo',
+        'id_hotel': 'bravo',
         'nome': 'Bravo Hotel',
         'estrelas': 2.3,
         'diaria': 420.34,
         'cidade': 'São Paulo'
     },
     {
-        'hotel_id': 'delta',
+        'id_hotel': 'delta',
         'nome': 'Delta Hotel',
         'estrelas': 3.3,
         'diaria': 420.34,
@@ -28,3 +29,28 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+    
+
+nomes = []
+for i in range(len(hoteis)):
+    nomes.append(hoteis[i]['nome'])
+class Lista_Hoteis(Resource):
+    def get(self):
+        return nomes
+            
+    
+class Hotel(Resource):
+    def get(self, id_hotel):
+        for hotel in hoteis:
+            if hotel['id_hotel'] == id_hotel:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404    
+    
+    def post(self, id_hotel):
+        pass
+    
+    def put(self, id_hotel):
+        pass
+    
+    def delete(self, id_hotel):
+        pass
